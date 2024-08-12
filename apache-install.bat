db6e22697e58801bcbbc6a7c56324aef3f4bb2bf
John <john@gis.tw>
2024-08-12 14:18:45 +0800

    更新安裝方式

--- apache-install.bat
+++ apache-install.bat
@@ -4,7 +4,10 @@ REM This installs and starts the apache service
 SET mypath=%~dp0
 echo %mypath%
 rd C:\ms4w_MSSQL
+rd C:\sqlite3_ext
 mklink /d C:\ms4w_MSSQL %mypath%ms4w_MSSQL
+mkdir c:\sqlite3_ext
+xcopy /-y  %mypath%ms4w_MSSQL\sqlite3_ext\*.* C:\sqlite3_ext\
 
 cd C:\ms4w_MSSQL\Apache\bin
 httpd -k install -n "Apache MS4W MSSQL Web Server: port 82"
